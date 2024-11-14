c37e2ccbb11ef3958998071a5a5ea7f2607e3d91
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2024-11-14 09:36:50 +0100

    updated generate-report to include time

--- generate-report.py
+++ generate-report.py
@@ -9,7 +9,7 @@ QUERY_TIMEOUT = "TIMEOUT"
 outputFiles = list(filter(lambda path : path.suffix == ".out", Path("./out/").iterdir()))
 outputFiles.sort()
 resultFile = open("results.csv", "w")
-resultFile.write("Net,Category,Index,Result\n")
+resultFile.write("Net,Category,Index,Result,Time\n")
 for outputPath in outputFiles:
     content = outputPath.read_text()
     matches = re.finditer("#{6}\\s+RUNNING\\s+([^_]+)_([^#\\.]+)\\.xml\\s+#{6}([^#]+)", content)
@@ -17,6 +17,10 @@ for outputPath in outputFiles:
         name = match.group(1)
         category = match.group(2)
         outContent = match.group(3)
+        time = "unknown"
+        timeMatch = re.search("real\\s*([0-9]+m[0-9]+\\.[0-9]+s)", outContent)
+        if (timeMatch != None):
+            time = timeMatch.group(1)
         status = ""
         if (QUERY_SATISFIED in outContent):
             status = "satisfied"
@@ -24,10 +28,11 @@ for outputPath in outputFiles:
             status = "unsatisfied"
         elif (QUERY_TIMEOUT in outContent):
             status = "timeout"
+            time = "n/a"
         else:
             status = "error"
         query_index = re.search("-x ([0-9]+)", outContent)
-        resultFile.write(f"{name},{category},{query_index.group(1)},{status}\n")
+        resultFile.write(f"{name},{category},{query_index.group(1)},{status},{time}\n")
         
         
 resultFile.close()
